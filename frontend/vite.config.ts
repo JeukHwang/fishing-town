73c461eb818da34e30e8840b81f68dfe364f040f
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2024-09-14 14:26:17 -0500

    feat: apply shadcn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  /* Custom for shadcn/ui */
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
