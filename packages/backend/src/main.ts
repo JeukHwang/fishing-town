50d03da6c6790b6e57624fe3ec46d3b1af84cc4e
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2024-12-23 04:52:50 -0500

    feat: lint all files

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -1,6 +1,6 @@
-import { NestFactory } from '@nestjs/core';
-import type { NestExpressApplication } from '@nestjs/platform-express';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import type { NestExpressApplication } from "@nestjs/platform-express";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
