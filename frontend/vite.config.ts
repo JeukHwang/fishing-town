a5a706fc0c32e1c94fd31fbf99fbfbaca7e93140
JeukHwang <92910273+JeukHwang@users.noreply.github.com>
2025-01-02 01:50:37 -0500

    feat: reorganize

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  /* Settings for shadcn/ui */
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "./src") },
+  },
+});
